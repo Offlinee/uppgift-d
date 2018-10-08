be3068f66170285a4f6daf93a8d049fac22a2471
Johnny.Chu <johnny.chu@elev.ga.lbs.se>
2018-10-08 10:53:54 +0200

    Init

---
+++ Assets/Skepp.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skepp : MonoBehaviour
+{
+    public SpriteRenderer rend;
+    public Color color;
+    public float rotationspeed = 360;
+    public float movespeed = 1f;
+   
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // svänger åt höger om man trycker på D
+        if (Input.GetKey(KeyCode.D)) 
+        {
+            transform.Rotate(0, 0, -rotationspeed * Time.deltaTime);
+            rend.color = Color.blue;
+        }
+        // svänger åt vänster Om man trycker på A
+        if (Input.GetKey(KeyCode.A)) 
+        {
+            transform.Rotate(0, 0, rotationspeed / 2 * Time.deltaTime);
+            rend.color = Color.green; 
+        }
+        // gör att båten hela tiden åker framåt
+        transform.Translate(0, -movespeed * Time.deltaTime, 0, Space.Self);
+        // sänker farten till hälften
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, movespeed/2 * Time.deltaTime, 0, Space.Self);
+        }
+        //gör att båten får en random färg när man trycker på space eller håller in i den
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rend.color = new Color(Random.Range(0.1f, 1f), Random.Range(0.1f, 1f), Random.Range(0.1f, 1f));
+        }
+
+    }
+}
---
+++ Assets/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float timerText;
+    private float startTime;
+    public float number = 1;
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // ökar med 1 varje sekund
+        timerText = timerText + Time.deltaTime; 
+        // om timerText är lika mycket som eller större än number så körs följande komandon
+        if (timerText >= number)
+        {
+            // om timerText = eller större än number så printar timer
+            print(string.Format("Timer:{0}", timerText));
+            // om timerText = eller större än number så ökar number med 1
+            number = number + 1;
+        }
+    }
+    }
