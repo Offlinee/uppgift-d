6811951d853df775c466a2c6cd72c57f69c9d031
Johnny.Chu <johnny.chu@elev.ga.lbs.se>
2018-10-10 21:37:23 +0200

    Random Spawn/Speed

--- Assets/Skepp.cs
+++ Assets/Skepp.cs
@@ -11,8 +11,11 @@ public class Skepp : MonoBehaviour
    
 	// Use this for initialization
 	void Start ()
+        // randomiserar farten 
     {
-		
+        movespeed = Random.Range(1, 11);
+        //spawnar vid ett random location
+        transform.Translate(Random.Range(-18, 19), Random.Range(-10, 11), 0);
 	}
 	
 	// Update is called once per frame
@@ -42,6 +45,25 @@ public class Skepp : MonoBehaviour
         {
             rend.color = new Color(Random.Range(0.1f, 1f), Random.Range(0.1f, 1f), Random.Range(0.1f, 1f));
         }
-
+        //om den åker utanför -18 x så hamnar den vid 18y
+        if (transform.position.x < -18)
+        {
+            transform.position = new Vector3(18, transform.position.y);
+        }
+        //om den är mer än 18x så hamnar den på -18y
+        if (transform.position.x > 18)
+        {
+            transform.position = new Vector3(-18, transform.position.y);
+        }
+        //om den är mindre än -10y så hamnar den på 10x
+        if (transform.position.y < -10)
+        {
+            transform.position = new Vector3(transform.position.x, 10);
+        }
+        //om den är mer än 10 så hamnar den på -10x
+        if (transform.position.y > 10)
+        {
+            transform.position = new Vector3(transform.position.x,- 10);
+        }
     }
 }
